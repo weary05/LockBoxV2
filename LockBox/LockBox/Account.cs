a4e9c26214a6756d02581a0503df283f7bfb3c31
evan bryce <evan.bryce.ejb@gmail.com>
2024-05-09 18:18:52 +0100

    initial

---
+++ LockBox/LockBox/Account.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LockBox
+{
+    class Account
+    {
+        public MailAddress Address { get; private set; }
+        public string AccountName { get; private set; }
+        public string Password { get; private set; }
+        public string ExtraNotes { get; private set; }
+
+        public Account(string AccountName, string Password, string ExtraNotes, string emailAddress)
+        {
+            this.AccountName = AccountName;
+            this.Password = Password;
+            this.ExtraNotes = ExtraNotes;
+            try
+            {
+                this.Address = new MailAddress(emailAddress);
+            }
+            catch { throw new Exception("Email address entered is not valid"); }
+        }
+    }
+}
---
+++ LockBox/LockBox/AccountHandler.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LockBox
+{
+    class AccountHandler
+    {
+        public List<Account> accounts { get; private set; }
+        public List<MailAddress> mailAddresses {  get; private set; }
+        public AccountHandler() 
+        {
+            accounts = new List<Account>();
+            mailAddresses = new List<MailAddress>();
+        }
+
+        public void AddAccount(string accountName, string emailAddress, string password, string extraNotes) 
+        { 
+            accounts.Add(new Account(accountName, password, extraNotes, emailAddress));
+        }
+        public void RemoveAccount(string accountName) 
+        {
+            foreach(Account account in accounts)
+            {
+                if(accountName == account.AccountName)
+                {
+                    accounts.Remove(account);
+                }
+            }
+        }
+    }
+}

598b4fa9736211f3679d2f46a3ef52bfb869aec6
evan bryce <evan.bryce.ejb@gmail.com>
2024-06-04 20:47:14 +0100

    Created basic GUI functionality, Can select and display accounts, still need to create ability to copy password to clipboard with click, to load and save data as well as protection against unintentional editing

--- LockBox/LockBox/MainWindow.xaml.cs
+++ LockBox/LockBox/MainWindow.xaml.cs
@@ -16,9 +16,47 @@ namespace LockBox
     /// </summary>
     public partial class MainWindow : Window
     {
+        private AccountHandler handler = new AccountHandler();
         public MainWindow()
         {
             InitializeComponent();
+            //temporary remove later
+            handler.AddAccount("name", "badkljcvobd@gmail.com", "b", "b");
+            //
+            Update(null, null);
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            System.Environment.Exit(0);
+        }
+
+        private void Update(object sender, RoutedEventArgs e)
+        {
+            foreach (Account account in handler.accounts) 
+            {
+                Button button = new Button();
+                button.Content = account.AccountName; button.Style = (Style)FindResource("ButtonStyle"); button.Click += OpenAccountData;
+                AccountList.Children.Add(button);
+            }
+        }
+
+        private void OpenAccountData(object sender, RoutedEventArgs e)
+        {
+            string name = sender.ToString();
+            name = name.Substring(name.IndexOf(" ") + 1);
+
+            //search through accounts for the one that matches the selected option and set account data for display.
+            foreach (Account account in handler.accounts)
+            {
+                if (account.AccountName == name)
+                {
+                    AccountName.Text = account.AccountName;
+                    EmailAddressBox.Text = account.Address.ToString();
+                    Password.Text = account.Password;
+                    ExtraDetails.Text = account.ExtraNotes;
+                }
+            }
         }
     }
 }
